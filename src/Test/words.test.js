698bc6cf07e2f97f8531960c7f8fdee7a248e0c8
Venla Viertola <venla.viertola@tuni.fi>
2020-12-06 18:49:17 +0200

    tests run

--- src/Test/words.test.js
+++ src/Test/words.test.js
@@ -1,6 +1,7 @@
 import words from "../words.js"
 
 
+
 describe("#words()", function() {
     test("Splits `string` into an array of its words.", function() {
         expect(words('fred, barney, & pebbles')).toStrictEqual(['fred', 'barney', 'pebbles'])
